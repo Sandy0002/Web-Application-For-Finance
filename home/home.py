cc553ddc314432ed235b8fbdbf9793705724cde5
P. Sandeep Murthy <110614803+Sandy0002@users.noreply.github.com>
2022-12-27 12:33:27 +0530

    Update home.py

--- home/home.py
+++ home/home.py
@@ -3,7 +3,7 @@ import streamlit as st
 from streamlit_lottie import st_lottie
 import requests as re
 
-
+# configuration settings
 hideStyle="""
     <style>
     #MainMenu {visibility: hidden;}
@@ -17,8 +17,7 @@ st.markdown(hideStyle,unsafe_allow_html=True)
 st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
 
 
-# ef5b10
-# add margin-left=800px for the li having Login
+
 st.markdown("""
 <nav class="navbar fixed-top navbar-expand-lg navbar-light" style="background-color:#7E0994">
   <a class="navbar-brand" href="#" target="_self"><b><h4>InvestEd</h4><b></a>
