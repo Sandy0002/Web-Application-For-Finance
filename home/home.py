ce10fd3953c09b5c839e36400a222f67a8cf0da1
P. Sandeep Murthy <110614803+Sandy0002@users.noreply.github.com>
2022-12-27 13:27:36 +0530

    Update home.py

--- home/home.py
+++ home/home.py
@@ -1,9 +1,8 @@
-
 import streamlit as st
 from streamlit_lottie import st_lottie
 import requests as re
 
-# configuration settings
+# Configuration settings
 hideStyle="""
     <style>
     #MainMenu {visibility: hidden;}
@@ -15,8 +14,6 @@ st.set_page_config(page_title="InvestEd",page_icon=":moneybag:",layout="wide")
 st.markdown(hideStyle,unsafe_allow_html=True)
 
 st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
-
-
 
 st.markdown("""
 <nav class="navbar fixed-top navbar-expand-lg navbar-light" style="background-color:#7E0994">
@@ -34,7 +31,7 @@ st.markdown("""
 </nav>
 """, unsafe_allow_html=True)
 
-
+# Gif setup
 def lott(url):
     r = re.get(url)
     if r.status_code!=200:
